9b50ba99bdb046d324a067d94a14277a9d01cdab
samsmoak <samsmoaky@gmail.com>
2026-02-23 18:30:11 -0500

    another commit

--- app/project1/page.tsx
+++ app/project1/page.tsx
@@ -31,7 +31,7 @@ export default function Project1() {
         <div className="flex flex-col items-end ">
         <h2 className="text-sm font-semibold ">Download </h2>
         <Link
-          href="/proposals/DBMS_PROJECT_PROPOSAL_B.pdf"
+          href="/proposals/DBMS_PROJECT_PROPOSAL_Z.pdf"
           download
           className="inline-flex items-center gap-2 text-primary hover:underline font-medium"
         >
